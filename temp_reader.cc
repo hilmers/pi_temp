6f2a3376204db29abccba7f1ca5c635f89dfae05
Kristoffer Hilmersson <kristoffer.hilmersson@gmail.com>
2020-11-18 19:51:50 +0000

    Created SQL statement for inserting temperature

--- temp_reader.cc
+++ temp_reader.cc
@@ -2,10 +2,11 @@
 #include <string>
 #include <iostream>
 #include <sqlite3.h>
+#include <chrono>
 
 constexpr auto pi_temperature_path = "/sys/class/thermal/thermal_zone0/temp";
 
-float GetCurrentTemp() {
+const float GetCurrentTemp() {
     std::ifstream temp_file(pi_temperature_path);
     std::string temperature_string((std::istreambuf_iterator<char>(temp_file)), std::istreambuf_iterator<char>());
     float temperature = std::stof(temperature_string);
@@ -16,13 +17,17 @@ float GetCurrentTemp() {
 int main() {
     sqlite3* db;
     int db_opened = sqlite3_open("data.db", &db);
-    if (db_opened != 0) {
+    if (db_opened != SQLITE_OK) {
         std::cout << "Failed to open database\n";
 	return 1;
     } else {
         std::cout << "Successfully opened database\n";
     }
-    auto temp = GetCurrentTemp();
+    const auto now = std::chrono::system_clock::now();
+    const auto epoch = now.time_since_epoch();
+    const auto temp = GetCurrentTemp();
+    std::string sql_statement = "INSERT INTO temperature (epoch_time, temperature) VALUES (" + std::to_string(epoch.count()) + ", " + std::to_string(temp) + ")";
+    std::cout << sql_statement << "\n";
     std::cout << temp << "\n";
     int db_closed = sqlite3_close(db);
     if (db_closed != 0)
